29819ff624ca36507d4946516596609672658c86
Stéphane THIERCY <sthiercy@gmail.com>
2020-11-14 08:49:07 +0100

    Sounds Brevity v2

--- Campagne_AAA_Master.lua
+++ Campagne_AAA_Master.lua
@@ -2,226 +2,226 @@
 -- INITIALISATIONS
 ---------------------------------------------------------------------------------------------------
 
--- Settings MOOSE
+    -- Settings MOOSE
 
-_SETTINGS:SetPlayerMenuOff()
-RAT.ATCswitch = false
+    _SETTINGS:SetPlayerMenuOff()
+    RAT.ATCswitch = false
 
--- Inits
+    -- Inits
 
-local EnvProd = false
+    local EnvProd = false
 
-if EnvProd == false then
-    MessageToAll("DEVELOPPEMENT", 120)
-    TacticalDisplay = true
-end
+    if EnvProd == false then
+        MessageToAll("DEVELOPPEMENT", 120)
+        TacticalDisplay = true
+    end
 
-local DangerZone = {}
-DangerZone.S300 = 125000
-DangerZone.Hawk = 50000
+    local DangerZone = {}
+    DangerZone.S300 = 125000
+    DangerZone.Hawk = 50000
 
-local Country = {}
-Country.IsraelOTAN = {"USA", "ISRAEL"}
-Country.TurkeySyria = {"TURQEY", "CJTF_BLUE"}
+    local Country = {}
+    Country.IsraelOTAN = {"USA", "ISRAEL"}
+    Country.TurkeySyria = {"TURQEY", "CJTF_BLUE"}
 
-local RadioGeneral = 305.00
+    local RadioGeneral = 305.00
 
-local Tanker = {}
-Tanker.Boom = 0 -- perche
-Tanker.Probe = 1 -- panier
+    local Tanker = {}
+    Tanker.Boom = 0 -- perche
+    Tanker.Probe = 1 -- panier
 
--- Border Zones
+    -- Border Zones
 
-local BORDER    = {}
-BORDER.Blue     = {}
+    local BORDER    = {}
+    BORDER.Blue     = {}
 
-BORDER.Red          = ZONE_POLYGON:New("BORDER_Red", GROUP:FindByName("BORDER_Red"))
-BORDER.Blue.Israel  = ZONE_POLYGON:New("BORDER_Blue_Israel", GROUP:FindByName("BORDER_Blue_Israel"))
-BORDER.Blue.Syria   = ZONE_POLYGON:New("BORDER_Blue_Syria", GROUP:FindByName("BORDER_Blue_Syria"))
-BORDER.Blue.Turkey  = ZONE_POLYGON:New("BORDER_Blue_Turkey", GROUP:FindByName("BORDER_Blue_Turkey"))
-BORDER.Blue.OTAN    = ZONE_POLYGON:New("BORDER_Blue_OTAN", GROUP:FindByName("BORDER_Blue_OTAN"))
+    BORDER.Red          = ZONE_POLYGON:New("BORDER_Red", GROUP:FindByName("BORDER_Red"))
+    BORDER.Blue.Israel  = ZONE_POLYGON:New("BORDER_Blue_Israel", GROUP:FindByName("BORDER_Blue_Israel"))
+    BORDER.Blue.Syria   = ZONE_POLYGON:New("BORDER_Blue_Syria", GROUP:FindByName("BORDER_Blue_Syria"))
+    BORDER.Blue.Turkey  = ZONE_POLYGON:New("BORDER_Blue_Turkey", GROUP:FindByName("BORDER_Blue_Turkey"))
+    BORDER.Blue.OTAN    = ZONE_POLYGON:New("BORDER_Blue_OTAN", GROUP:FindByName("BORDER_Blue_OTAN"))
 
--- Border Smoke pour rigoler
-if EnvProd == false then
-    BORDER.Red:SmokeZone(SMOKECOLOR.Red)
-    BORDER.Blue.Israel:SmokeZone(SMOKECOLOR.Blue)
-    BORDER.Blue.Syria:SmokeZone(SMOKECOLOR.Blue)
-    BORDER.Blue.Turkey:SmokeZone(SMOKECOLOR.Blue)
-    BORDER.Blue.OTAN:SmokeZone(SMOKECOLOR.Blue)
-end
+    -- Border Smoke pour rigoler
+    if EnvProd == false then
+        BORDER.Red:SmokeZone(SMOKECOLOR.Red)
+        BORDER.Blue.Israel:SmokeZone(SMOKECOLOR.Blue)
+        BORDER.Blue.Syria:SmokeZone(SMOKECOLOR.Blue)
+        BORDER.Blue.Turkey:SmokeZone(SMOKECOLOR.Blue)
+        BORDER.Blue.OTAN:SmokeZone(SMOKECOLOR.Blue)
+    end
 
--- SAM Groups / Commenter un site SAM pour le désactiver
+    -- SAM Groups / Commenter un site SAM pour le désactiver
 
-local SAM = {}
-SAM.Blue        = {}
-SAM.Red         = {}
-SAM.Blue.Israel = {}
-SAM.Blue.Syria  = {}
-SAM.Blue.Turkey = {}
-SAM.Red.Lebanon = {}
-SAM.Red.Syria   = {}
+    local SAM = {}
+    SAM.Blue        = {}
+    SAM.Red         = {}
+    SAM.Blue.Israel = {}
+    SAM.Blue.Syria  = {}
+    SAM.Blue.Turkey = {}
+    SAM.Red.Lebanon = {}
+    SAM.Red.Syria   = {}
 
-SAM.Blue.Israel.Haifa_Patriot   = GROUP:FindByName("SAM_Blue_Israel_Haifa_Patriot"):Activate()
-SAM.Blue.Israel.Megiddo_Patriot = GROUP:FindByName("SAM_Blue_Israel_Megiddo_Patriot"):Activate()
-SAM.Blue.Syria.Idlib_Hawk       = GROUP:FindByName("SAM_Blue_Syria_Idlib_Hawk"):Activate()
-SAM.Blue.Syria.Hama_Hawk        = GROUP:FindByName("SAM_Blue_Syria_Hama_Hawk"):Activate()
-SAM.Blue.Syria.Aleppo_S300      = GROUP:FindByName("SAM_Blue_Syria_Aleppo_S300"):Activate()
-SAM.Blue.Syria.Palmyra_Hawk     = GROUP:FindByName("SAM_Blue_Syria_Palmyra_Hawk"):Activate()
-SAM.Blue.Syria.Tabqa_S300       = GROUP:FindByName("SAM_Blue_Syria_Tabqa_S300"):Activate()
-SAM.Blue.Turkey.CB22_S300       = GROUP:FindByName("SAM_Blue_Turkey_CB22_S300"):Activate()
-SAM.Blue.Turkey.DB30_S300       = GROUP:FindByName("SAM_Blue_Turkey_DB30_S300"):Activate()
-SAM.Blue.Turkey.IF25_S300       = GROUP:FindByName("SAM_Blue_Turkey_IF25_S300"):Activate()
-SAM.Red.Syria.AlQusayr_Hawk     = GROUP:FindByName("SAM_Red_Syria_AlQusayr_Hawk"):Activate()
-SAM.Red.Syria.Damascus_S300     = GROUP:FindByName("SAM_Red_Syria_Damascus_S300"):Activate()
-SAM.Red.Lebanon.Beirut_Hawk     = GROUP:FindByName("SAM_Red_Lebanon_Beirut_Hawk"):Activate()
+    SAM.Blue.Israel.Haifa_Patriot   = GROUP:FindByName("SAM_Blue_Israel_Haifa_Patriot"):Activate()
+    SAM.Blue.Israel.Megiddo_Patriot = GROUP:FindByName("SAM_Blue_Israel_Megiddo_Patriot"):Activate()
+    SAM.Blue.Syria.Idlib_Hawk       = GROUP:FindByName("SAM_Blue_Syria_Idlib_Hawk"):Activate()
+    SAM.Blue.Syria.Hama_Hawk        = GROUP:FindByName("SAM_Blue_Syria_Hama_Hawk"):Activate()
+    SAM.Blue.Syria.Aleppo_S300      = GROUP:FindByName("SAM_Blue_Syria_Aleppo_S300"):Activate()
+    SAM.Blue.Syria.Palmyra_Hawk     = GROUP:FindByName("SAM_Blue_Syria_Palmyra_Hawk"):Activate()
+    SAM.Blue.Syria.Tabqa_S300       = GROUP:FindByName("SAM_Blue_Syria_Tabqa_S300"):Activate()
+    SAM.Blue.Turkey.CB22_S300       = GROUP:FindByName("SAM_Blue_Turkey_CB22_S300"):Activate()
+    SAM.Blue.Turkey.DB30_S300       = GROUP:FindByName("SAM_Blue_Turkey_DB30_S300"):Activate()
+    SAM.Blue.Turkey.IF25_S300       = GROUP:FindByName("SAM_Blue_Turkey_IF25_S300"):Activate()
+    SAM.Red.Syria.AlQusayr_Hawk     = GROUP:FindByName("SAM_Red_Syria_AlQusayr_Hawk"):Activate()
+    SAM.Red.Syria.Damascus_S300     = GROUP:FindByName("SAM_Red_Syria_Damascus_S300"):Activate()
+    SAM.Red.Lebanon.Beirut_Hawk     = GROUP:FindByName("SAM_Red_Lebanon_Beirut_Hawk"):Activate()
 
--- SAM Zones
+    -- SAM Zones
 
-local SAMzone = {}
+    local SAMzone = {}
 
-if SAM.Red.Syria.AlQusayr_Hawk then SAMzone.AlQusayr_Hawk = ZONE_GROUP:New("Zone_AlQusayr_Hawk", SAM.Red.Syria.AlQusayr_Hawk, DangerZone.Hawk) end
-if SAM.Red.Syria.Damascus_S300 then SAMzone.Damascus_S300 = ZONE_GROUP:New("Zone_Damascus_S300", SAM.Red.Syria.Damascus_S300, DangerZone.S300) end
-if SAM.Red.Lebanon.Beirut_Hawk then SAMzone.Beirut_Hawk = ZONE_GROUP:New("Zone_Beirut_Hawk", SAM.Red.Lebanon.Beirut_Hawk, DangerZone.Hawk) end
+    if SAM.Red.Syria.AlQusayr_Hawk then SAMzone.AlQusayr_Hawk = ZONE_GROUP:New("Zone_AlQusayr_Hawk", SAM.Red.Syria.AlQusayr_Hawk, DangerZone.Hawk) end
+    if SAM.Red.Syria.Damascus_S300 then SAMzone.Damascus_S300 = ZONE_GROUP:New("Zone_Damascus_S300", SAM.Red.Syria.Damascus_S300, DangerZone.S300) end
+    if SAM.Red.Lebanon.Beirut_Hawk then SAMzone.Beirut_Hawk = ZONE_GROUP:New("Zone_Beirut_Hawk", SAM.Red.Lebanon.Beirut_Hawk, DangerZone.Hawk) end
 
--- NAVAL Groups
+    -- NAVAL Groups
 
-local NAVAL = {}
-NAVAL.Blue  = {}
-NAVAL.Red   = {}
+    local NAVAL = {}
+    NAVAL.Blue  = {}
+    NAVAL.Red   = {}
 
-NAVAL.Blue.Cyprus_OTAN = GROUP:FindByName("NAVAL_Blue_Cyprus_OTAN"):Activate()
+    NAVAL.Blue.Cyprus_OTAN = GROUP:FindByName("NAVAL_Blue_Cyprus_OTAN"):Activate()
 
--- Air Groups
+    -- Air Groups
 
-local AIR   = {}
-AIR.Red     = {}
-AIR.Blue    = {}
+    local AIR   = {}
+    AIR.Red     = {}
+    AIR.Blue    = {}
 
-AIR.Red.All             = SET_GROUP:New():FilterCoalitions("red"):FilterCategoryAirplane():FilterStart()
-AIR.Red.GCICAP          = SET_GROUP:New():FilterCoalitions("red"):FilterCategoryAirplane():FilterPrefixes{"CAP_Red", "GCI_Red"}:FilterStart()
-AIR.Blue.All            = SET_GROUP:New():FilterCoalitions("blue"):FilterCategoryAirplane():FilterStart()
-AIR.Blue.IsraelOTAN     = SET_GROUP:New():FilterCoalitions("blue"):FilterCountries(Country.IsraelOTAN):FilterCategoryAirplane():FilterStart()
-AIR.Blue.TurkeySyria    = SET_GROUP:New():FilterCoalitions("blue"):FilterCountries(Country.TurkeySyria):FilterCategoryAirplane():FilterStart()
+    AIR.Red.All             = SET_GROUP:New():FilterCoalitions("red"):FilterCategoryAirplane():FilterStart()
+    AIR.Red.GCICAP          = SET_GROUP:New():FilterCoalitions("red"):FilterCategoryAirplane():FilterPrefixes{"CAP_Red", "GCI_Red"}:FilterStart()
+    AIR.Blue.All            = SET_GROUP:New():FilterCoalitions("blue"):FilterCategoryAirplane():FilterStart()
+    AIR.Blue.IsraelOTAN     = SET_GROUP:New():FilterCoalitions("blue"):FilterCountries(Country.IsraelOTAN):FilterCategoryAirplane():FilterStart()
+    AIR.Blue.TurkeySyria    = SET_GROUP:New():FilterCoalitions("blue"):FilterCountries(Country.TurkeySyria):FilterCategoryAirplane():FilterStart()
 
--- GCI Detection Sets
+    -- GCI Detection Sets
 
-local EWR = {}
+    local EWR = {}
 
-EWR.Red     = SET_GROUP:New():FilterPrefixes({"SAM_Red", "EWR_Red"}):FilterStart()
-EWR.Israel  = SET_GROUP:New():FilterPrefixes({"SAM_Blue_Israel"}):FilterStart()
-EWR.OTAN    = SET_GROUP:New():FilterPrefixes({"NAVAL_Blue_Cyprus_OTAN", "AWACS_Blue_OTAN"}):FilterStart()
-EWR.Turkey  = SET_GROUP:New():FilterPrefixes({"SAM_Blue_Turkey"}):FilterStart()
-EWR.Syria   = SET_GROUP:New():FilterPrefixes({"SAM_Blue_Syria"}):FilterStart()
+    EWR.Red     = SET_GROUP:New():FilterPrefixes({"SAM_Red", "EWR_Red"}):FilterStart()
+    EWR.Israel  = SET_GROUP:New():FilterPrefixes({"SAM_Blue_Israel"}):FilterStart()
+    EWR.OTAN    = SET_GROUP:New():FilterPrefixes({"NAVAL_Blue_Cyprus_OTAN", "AWACS_Blue_OTAN"}):FilterStart()
+    EWR.Turkey  = SET_GROUP:New():FilterPrefixes({"SAM_Blue_Turkey"}):FilterStart()
+    EWR.Syria   = SET_GROUP:New():FilterPrefixes({"SAM_Blue_Syria"}):FilterStart()
 
 ---------------------------------------------------------------------------------------------------
 -- BORDER SAM LOGIC
 ---------------------------------------------------------------------------------------------------
 
---[[
+    --[[
 
-    ■ Blue SAMs
+        ■ Blue SAMs
 
-    OTAN / ISRAEL / TURKEY
-    Border OpenFire
+        OTAN / ISRAEL / TURKEY
+        Border OpenFire
 
-    ISIS / OPPOSITON :
-    OpenFire ASAP
+        ISIS / OPPOSITON :
+        OpenFire ASAP
 
-    ■ Red SAMs
+        ■ Red SAMs
 
-    Vs OTAN / ISRAEL
-    OpenFire ASAP
+        Vs OTAN / ISRAEL
+        OpenFire ASAP
 
-]]--
+    ]]--
 
-SchedulerBorderDefenses = SCHEDULER:New( nil,
-    function ()
+    SchedulerBorderDefenses = SCHEDULER:New( nil,
+        function ()
 
-        -- BORDER Blues
-        local function BlueSamBorderDefense (sam, border, bandits)
-            -- Autorisation de tir pour les SAM/Naval Bleus en fonction de la présence de bandits à l'intérieur de la frontière
-            if sam then
-                if bandits:AnyInZone(border) then
-                    if EnvProd == false then MessageToRed("DANGER ZONE", 2) end
-                    sam:OptionROEOpenFire()
-                else
-                    sam:OptionROEHoldFire()
+            -- BORDER Blues
+            local function BlueSamBorderDefense (sam, border, bandits)
+                -- Autorisation de tir pour les SAM/Naval Bleus en fonction de la présence de bandits à l'intérieur de la frontière
+                if sam then
+                    if bandits:AnyInZone(border) then
+                        if EnvProd == false then MessageToRed("DANGER ZONE", 2) end
+                        sam:OptionROEOpenFire()
+                    else
+                        sam:OptionROEHoldFire()
+                    end
                 end
             end
-        end
 
-        if AIR.Red.All:CountAlive() >= 1 then
-            BlueSamBorderDefense(SAM.Blue.Israel.Haifa_Patriot, BORDER.Blue.Israel, AIR.Red.All)
-            BlueSamBorderDefense(SAM.Blue.Israel.Megiddo_Patriot, BORDER.Blue.Israel, AIR.Red.All)
-            BlueSamBorderDefense(SAM.Blue.Turkey.CB22_S300, BORDER.Blue.Turkey, AIR.Red.All)
-            BlueSamBorderDefense(SAM.Blue.Turkey.DB30_S300, BORDER.Blue.Turkey, AIR.Red.All)
-            BlueSamBorderDefense(SAM.Blue.Turkey.IF25_S300, BORDER.Blue.Turkey, AIR.Red.All)
-            BlueSamBorderDefense(NAVAL.Blue.Cyprus_OTAN, BORDER.Blue.OTAN, AIR.Red.All)
-        end
+            if AIR.Red.All:CountAlive() >= 1 then
+                BlueSamBorderDefense(SAM.Blue.Israel.Haifa_Patriot, BORDER.Blue.Israel, AIR.Red.All)
+                BlueSamBorderDefense(SAM.Blue.Israel.Megiddo_Patriot, BORDER.Blue.Israel, AIR.Red.All)
+                BlueSamBorderDefense(SAM.Blue.Turkey.CB22_S300, BORDER.Blue.Turkey, AIR.Red.All)
+                BlueSamBorderDefense(SAM.Blue.Turkey.DB30_S300, BORDER.Blue.Turkey, AIR.Red.All)
+                BlueSamBorderDefense(SAM.Blue.Turkey.IF25_S300, BORDER.Blue.Turkey, AIR.Red.All)
+                BlueSamBorderDefense(NAVAL.Blue.Cyprus_OTAN, BORDER.Blue.OTAN, AIR.Red.All)
+            end
 
-        -- BORDER Reds
-        local function RedSamBorderDefense(sam, zonesam)
-            -- Autorisation de tir pour les SAM rouges en fonction de la présence de bandits à l'intérieur de la frontière
-            -- ou de la nationalité des bandits à l'intérieur de la zone de tir du SAM
-            if sam then
-                if AIR.Blue.IsraelOTAN:AnyInZone(BORDER.Red) or AIR.Blue.TurkeySyria:AnyInZone(zonesam) then
-                    if EnvProd == false then MessageToBlue("DANGER ZONE", 2) end
-                    sam:OptionROEOpenFire()
-                else
-                    sam:OptionROEHoldFire()
+            -- BORDER Reds
+            local function RedSamBorderDefense(sam, zonesam)
+                -- Autorisation de tir pour les SAM rouges en fonction de la présence de bandits à l'intérieur de la frontière
+                -- ou de la nationalité des bandits à l'intérieur de la zone de tir du SAM
+                if sam then
+                    if AIR.Blue.IsraelOTAN:AnyInZone(BORDER.Red) or AIR.Blue.TurkeySyria:AnyInZone(zonesam) then
+                        if EnvProd == false then MessageToBlue("DANGER ZONE", 2) end
+                        sam:OptionROEOpenFire()
+                    else
+                        sam:OptionROEHoldFire()
+                    end
                 end
             end
-        end
 
-        if AIR.Blue.All:CountAlive() >= 1 then
-            RedSamBorderDefense(SAM.Red.Syria.AlQusayr_Hawk, SAMzone.AlQusayr_Hawk)
-            RedSamBorderDefense(SAM.Red.Syria.Damascus_S300, SAMzone.Damascus_S300)
-            RedSamBorderDefense(SAM.Red.Lebanon.Beirut_Hawk, SAMzone.Beirut_Hawk)
-        end
-	end, {}, 1, 10
-)
+            if AIR.Blue.All:CountAlive() >= 1 then
+                RedSamBorderDefense(SAM.Red.Syria.AlQusayr_Hawk, SAMzone.AlQusayr_Hawk)
+                RedSamBorderDefense(SAM.Red.Syria.Damascus_S300, SAMzone.Damascus_S300)
+                RedSamBorderDefense(SAM.Red.Lebanon.Beirut_Hawk, SAMzone.Beirut_Hawk)
+            end
+        end, {}, 1, 10
+    )
 
 ---------------------------------------------------------------------------------------------------
 -- BREVITY RED GCICAP
 ---------------------------------------------------------------------------------------------------
 
-function AIR.Red.GCICAP:OnAfterAdded (From, Event, To, ObjectName, Object)
-    local NewGroup = GROUP:FindByName(ObjectName)
-    NewGroup:HandleEvent(EVENTS.Shot)
-    function NewGroup:OnEventShot (EventData)
-        local Brevity = "none"
-        local WeaponDesc = EventData.Weapon:getDesc() -- https://wiki.hoggitworld.com/view/DCS_enum_weapon
-        if WeaponDesc.category == 3 then
-            Brevity = "Pickle"
-        elseif WeaponDesc.category == 1 then
-            if WeaponDesc.guidance == 1 then
-                if WeaponDesc.missileCategory == 4 then
-                    Brevity = "LongRifle"
-                elseif WeaponDesc.missileCategory == 6 then
-                    Brevity = "Rifle"
+    function AIR.Red.GCICAP:OnAfterAdded (From, Event, To, ObjectName, Object)
+        local NewGroup = GROUP:FindByName(ObjectName)
+        NewGroup:HandleEvent(EVENTS.Shot)
+        function NewGroup:OnEventShot (EventData)
+            local Brevity = "none"
+            local WeaponDesc = EventData.Weapon:getDesc() -- https://wiki.hoggitworld.com/view/DCS_enum_weapon
+            if WeaponDesc.category == 3 then
+                Brevity = "Pickle_" .. math.random(2)
+            elseif WeaponDesc.category == 1 then
+                if WeaponDesc.guidance == 1 then
+                    if WeaponDesc.missileCategory == 4 then
+                        Brevity = "LongRifle_" .. math.random(2)
+                    elseif WeaponDesc.missileCategory == 6 then
+                        Brevity = "Rifle_" .. math.random(2)
+                    end
+                elseif WeaponDesc.guidance == 2 then
+                    Brevity = "Fox2_" .. math.random(2)
+                elseif WeaponDesc.guidance == 3 then
+                    Brevity = "Fox3_" .. math.random(2)
+                elseif WeaponDesc.guidance == 4 then
+                    Brevity = "Fox1_" .. math.random(2)
+                elseif WeaponDesc.guidance == 5 and WeaponDesc.missileCategory == 6 then
+                    Brevity = "Magnum_" .. math.random(2)
+                elseif WeaponDesc.guidance == 7 then
+                    Brevity = "Rifle_" .. math.random(2)
                 end
-            elseif WeaponDesc.guidance == 2 then
-                Brevity = "Fox2"
-            elseif WeaponDesc.guidance == 3 then
-                Brevity = "Fox3"
-            elseif WeaponDesc.guidance == 4 then
-                Brevity = "Fox1"
-            elseif WeaponDesc.guidance == 5 and WeaponDesc.missileCategory == 6 then
-                Brevity = "Magnum"
-            elseif WeaponDesc.guidance == 7 then
-                Brevity = "Rifle"
+            end
+            if Brevity ~= "none"  then
+                local BrevitySound = Brevity .. ".ogg"
+                local GroupRadio = NewGroup:GetRadio()
+                GroupRadio:SetFileName(BrevitySound)
+                GroupRadio:SetFrequency(RadioGeneral)
+                GroupRadio:SetModulation(radio.modulation.AM)
+                GroupRadio:Broadcast()
             end
         end
-        if Brevity ~= "none"  then
-            local BrevitySound = Brevity .. ".ogg"
-            local GroupRadio = NewGroup:GetRadio()
-            GroupRadio:SetFileName(BrevitySound)
-            GroupRadio:SetFrequency(RadioGeneral)
-            GroupRadio:SetModulation(radio.modulation.AM)
-            GroupRadio:Broadcast()
-        end
     end
-end
 
 ---------------------------------------------------------------------------------------------------
 -- BORDER CAP/GCI LOGIC
@@ -436,3 +436,9 @@ end
     A2ADispatcherRed:SetSquadronTakeoffFromRunway("Red GCIS")
     A2ADispatcherRed:SetSquadronGrouping("Red GCIS", 2)
     A2ADispatcherRed:SetSquadronGci("Red GCIS", 1000, 2000)
+
+
+---------------------------------------------------------------------------------------------------
+-- MISSION 01
+---------------------------------------------------------------------------------------------------
+
